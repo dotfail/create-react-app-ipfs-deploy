8216f0471380b46ffcbbf3f0258a302907764e22
dotfail <61684445+dotfail@users.noreply.github.com>
2022-02-01 23:38:21 -0800

    remove &copy;

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          &copy 2022 MetaChapel
+          MetaChapel.
         </a>
       </header>
     </div>
