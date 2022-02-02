bf7b1e0a0ac0476ffa36ad5dadff712dc91c6892
dotfail <61684445+dotfail@users.noreply.github.com>
2022-02-02 05:41:41 -0800

    different skybox

--- src/index.js
+++ src/index.js
@@ -5,53 +5,33 @@ import 'babel-polyfill';
 import {Entity, Scene} from 'aframe-react';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import logoTexture from './images/metachapel-welcome.png';
+import skyTexture from './videos/sky.mp4';
 import floorTexture from './images/floor.jpg';
-import skyTexture from './images/sky.jpg';
+import cubeSculpture from './sculptures/sculpture.glb';
 
 
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {color: 'red'};
-    }
 
-    changeColor() {
-        const colors = ['red', 'orange', 'yellow', 'green', 'blue'];
-        this.setState({
-            color: colors[Math.floor(Math.random() * colors.length)]
-        });
     }
 
     render () {
         return (
             <Scene>
                 <a-assets>
+                    <img id="sky" alt="sky" src={skyTexture} />
                     <img id="groundTexture" alt="floor texture" src={floorTexture} />
-                    <img id="skyTexture" alt="sky texture" src={skyTexture} />
+                    <img id="pdx" alt="logo" src={logoTexture} />
+                    <a-asset-item id="sculpture" src={cubeSculpture}></a-asset-item>
                 </a-assets>
-
                 <Entity primitive="a-plane" src="#groundTexture" rotation="-90 0 0" height="100" width="100"/>
-                <Entity primitive="a-light" type="ambient" color="#445451"/>
-                <Entity primitive="a-light" type="point" intensity="2" position="2 4 4"/>
-                <Entity primitive="a-sky" height="2048" radius="30" src="#skyTexture" theta-length="90" width="2048"/>
-                <Entity particle-system={{preset: 'snow', particleCount: 2000}}/>
-                <Entity text={{value: 'MetaChapel welcomes you.', align: 'center'}} position={{x: 0, y: 2, z: -1}}/>
-
-                <Entity id="box"
-                        geometry={{primitive: 'box'}}
-                        material={{color: this.state.color, opacity: 0.6}}
-                        animation__rotate={{property: 'rotation', dur: 2000, loop: true, to: '360 360 360'}}
-                        animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '1.1 1.1 1.1'}}
-                        position={{x: 0, y: 1, z: -3}}
-                        events={{click: this.changeColor.bind(this)}}>
-                    <Entity animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '2 2 2'}}
-                            geometry={{primitive: 'box', depth: 0.2, height: 0.2, width: 0.2}}
-                            material={{color: '#24CAFF'}}/>
-                </Entity>
-
-                <Entity primitive="a-camera">
-                    <Entity primitive="a-cursor" animation__click={{property: 'scale', startEvents: 'click', from: '0.1 0.1 0.1', to: '1 1 1', dur: 150}}/>
-                </Entity>
+                <Entity particle-system={{preset: 'dust', color: '#0000FF,#FF0000', particleCount: 20000}}/>
+                <a-sky src="#sky"/>
+                <a-gltf-model src="#sculpture" position="0 0 -6"></a-gltf-model>
+                <a-image src="#pdx" width="10" height="5" position="-2 1.2 -3"
+    scale="0.2 0.2 0.2"/>
             </Scene>
         );
     }
